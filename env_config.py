f791589bea3bf12b56bcd35b4dd9b54698e8177a
Olivia Perryman <omp@grouse.cs.ubc.ca>
2022-10-10 15:51:16 -0700

    Change path to lmdb root

--- env_config.py
+++ env_config.py
@@ -1,8 +1,8 @@
 LMDB_ROOTS = {
     # Definition:
     #     str(socket.gethostname()): "<path/to/lmdb/to/be/placed>"
-    "my-server": "/data/hubert/data/infinityGAN-lmdb/",
-    "my-PC"    : "/mnt/merced-pc/infinityGAN/data/infinityGAN-lmdb/",
+    # "my-server": "/local/omp/data/infinityGAN-lmdb/",
+    "my-PC"    : "/local/omp/data/infinityGAN-lmdb/",
     # "OuO"       :    "/mnt/hubert/data/infinityGAN-lmdb/",
     "unspecified": [
         # list paths here is don't wanna specify the server name, but have risks in popping unhandled errors.
